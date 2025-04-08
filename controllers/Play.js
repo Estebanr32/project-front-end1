fb671609485e14b2f3d7ca35e117f14155551f16
Svelezmo <sebasvm59@gmail.com>
2025-04-08 08:51:23 -0500

    revision play

--- controllers/Play.js
+++ controllers/Play.js
@@ -4,12 +4,14 @@ import { ContainerShips } from "../Model/containerShips.js";
 import { fetchJson, numberRamdonBetween } from "../Helpers/Utils.js";
 import { fetchPage } from "../Helpers/Utils.js";
 import { raking } from "./ranking.js";
+import { App } from "./App.js";
 export async function play() {
     localStorage.setItem("isShiftPlayer", true)
+    const casillasSeleccionadasMaquinas = [];
+    localStorage.setItem("casillasSeleccionadasMaquinas", JSON.stringify(casillasSeleccionadasMaquinas))
     const size = parseInt(localStorage.getItem("size"))
     const container = document.getElementById("tableroJugador")
     chargeWeather()
-    chargeUser()
 
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -18,15 +20,19 @@ export async function play() {
 
 
 
-    createMap("p2", document.getElementById("tableroEnemigo"), (div) => {
-        div.onclick = () => {
-            let isImpactUser = stroke(div, ContainerShips.containerMachine, mapMachine)
+    createMap("p2", document.getElementById("tableroEnemigo"), async (div) => {
+        div.onclick = async () => {
+            chargeUser()
+            let isImpactUser = await stroke(div, ContainerShips.containerMachine, mapMachine)
 
             localStorage.setItem("isShiftPlayer", isImpactUser)
+            console.log(localStorage.getItem("isShiftPlayer"))
             while (localStorage.getItem("isShiftPlayer") == "false") {
+                console.log(ContainerShips.ContainerUser);
 
-                let isImpactMachine = strokeMachine()
-                localStorage.setItem("isShiftPlayer", isImpactMachine)
+                let isImpactMachine = await strokeMachine()
+                console.log(isImpactMachine);
+                localStorage.setItem("isShiftPlayer", !isImpactMachine)
             }
 
         }
@@ -40,10 +46,21 @@ async function strokeMachine() {
     const y = numberRamdonBetween(0, size - 1)
 
     const div = document.getElementById(`p1-${x}-${y}`)
-    return stroke(div, ContainerShips.ContainerUser, ContainerShips.matrixPlayer)
+    const casillasSeleccionadasMaquinas = JSON.parse(localStorage.getItem("casillasSeleccionadasMaquinas"));
+    casillasSeleccionadasMaquinas.forEach(() => {
+        if (element[0] == x && element[1] == y) {
+            return true
+
+        }
+    })
+
+    casillasSeleccionadasMaquinas.push([x, y])
+
+    return await stroke(div, ContainerShips.ContainerUser, ContainerShips.matrixPlayer)
 }
 
-function stroke(div, listOponent, matrixOponente) {
+ async function stroke(div, listOponent, matrixOponente) {
+
     const id = div.id
     const separateId = id.split("-")
     const user = separateId[0]
@@ -52,17 +69,24 @@ function stroke(div, listOponent, matrixOponente) {
     let isWinner = false
     let isImpact = false
 
+    if (user == "p1") {
+
+    }
     listOponent.forEach(ship => {
-
+       
         if (existeBarco(ship.getPositions(), [x, y])) {
+            if (user == "p2") {
+                div.onclick=()=>{
 
+                    alert("Esta bomba ya estallo")
+                    return 
+                }
+    
+            }
             deletePosition(ship, [x, y])
             isImpact = true
-            if (user == "p2") {
-                div.onclick = () => {
-                    alert("Esta poscicion ya ha sido atacada");
-                }
-            }
+
+
             matrixOponente[x][y] += "h"
             div.style.backgroundColor = "black"
             if (ship.getPositions().length == 0) {
@@ -77,25 +101,69 @@ function stroke(div, listOponent, matrixOponente) {
 
 
         }
-        if (isWinner) {
-            //Aqui se hace el llamdo a la api
-        }
     })
-    if (!isImpact) {
-        //     console.log("hola");
+    if (user == "p2") {
+        const newScore = calculateScore(matrixOponente, x, y)
 
-        div.style.backgroundColor = "white"
-        return false
+        const user = JSON.parse(localStorage.getItem("user"));
+        const nickname = user["nick_name"];
+        const country = user["country_code"];
+        (user["score"]) = parseInt(user["score"]) + newScore;
 
-
-    } else {
-
-
+        localStorage.setItem("user", JSON.stringify(user))
+        chargeUser()
 
 
     }
+    if (isWinner) {
+        const user=localStorage.getItem("user")
+        user["score"] = parseInt(usuario["score"])
+        await fetch("http://127.0.0.1:5000/score-recorder", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+        localStorage.clear()
+        App.main()
+    }
+    if (!isImpact) {
+
+        div.style.backgroundColor = "white"
+        return false
+    }
+
+
+    return true
 };
+
+function calculateScore(matrixOponent, x, y) {
+    let scores = -1
+    const tamaño = matrixOponent[0].length;
+
+    const coordenadasCerca = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
+    if (matrixOponent[[x]][y] != "a") {
+        scores = 10;
+    }
+    coordenadasCerca.forEach(element => {
+        let i = x + element[0]
+        let j = y + element[1]
+        if (i > 0 && j > 0 && i < tamaño && j < tamaño) {
+
+            if (matrixOponent[i][j] != "a") {
+                scores = -5
+            }
+        }
+    })
+    return scores
+
+
+}
+
 function existeBarco(listaPrincipal, objetivo) {
+    console.log(listaPrincipal);
+
     return listaPrincipal.some(
         par => par[0] === objetivo[0] && par[1] === objetivo[1]
     );
@@ -110,24 +178,25 @@ function deletePosition(ship, aim) {
 }
 export function chargeUser() {
     const user = JSON.parse(localStorage.getItem("user"));
+
     const nickname = user["nick_name"];
     // const country = user["country_code"];
     const score = user["score"];
-    
+
     document.getElementById("user").innerText = nickname;
     // document.getElementById("country").innerText = country;
     document.getElementById("puntaje-valor").innerText = score;
 }
 export async function chargeWeather() {
     const API_KEY = "60c90e172eb6b3e0c4ea4d2956b0189b";
-    const city=localStorage.getItem("zoneBattle")
+    const city = localStorage.getItem("zoneBattle")
     const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${API_KEY}`)).json()
 
-    
-    const temperatura = data.main.temp;
-    
 
-    document.getElementById("clima").innerText = temperatura+"°";
- 
+    const temperatura = data.main.temp;
+
+
+    document.getElementById("clima").innerText = temperatura + "°";
+
 }
 
--- controllers/settingsUser.js
+++ controllers/settingsUser.js
@@ -3,17 +3,29 @@ import { fetchPage } from "../Helpers/Utils.js";
 import { play } from "./Play.js";
 import { ContainerShips } from "../Model/containerShips.js";
 import { matrixMap } from "../Helpers/Utils.js";
+import { raking } from "./ranking.js"
+import { Ship } from "../Model/Ship.js";
 
 
 export async function settingsUSer() {
     localStorage.setItem("rotateShip", false)
     await fetchPage("../views/settings.html", document.getElementById("container"))
+    const ranking = document.getElementById("ranking")
+
+    const container1 = document.getElementById("container")
+
+
+    ranking.addEventListener("click", async () => {
+        container1.innerHTML = ""
+        await fetchPage("../views/ranking.html", container1)
+        raking()
+
+    })
     const rotateMap = document.getElementById("rotarBarco")
 
     rotateMap.addEventListener("click", () => {
 
         localStorage.setItem("rotateShip", localStorage.getItem("rotateShip") == "true" ? false : true)
-        console.log(localStorage.getItem("rotateShip"));
 
     })
     ContainerShips.matrixPlayer = matrixMap()
@@ -36,34 +48,35 @@ async function drawShip(div) {
     const select = document.getElementById("seleccionBarco")
     const optionShip = select.options[select.selectedIndex]
     const sizeShip = parseInt(optionShip.getAttribute("data-size"))
-    const selectColor=document.getElementById("seleccionColor")
-    const optionColor=selectColor.options[selectColor.selectedIndex]
-    const color=optionColor.getAttribute("data-code")
+    const selectColor = document.getElementById("seleccionColor")
+    const optionColor = selectColor.options[selectColor.selectedIndex]
+    const color = optionColor.getAttribute("data-code")
 
     let id = (div.id).split("-")
     const x = parseInt(id[1])
     const y = parseInt(id[2])
     const rotateShip = localStorage.getItem("rotateShip")
-    console.log(rotateShip);
-
+    let ship=new Ship()
     if (rotateShip == "true") {
         if (sizeShip + x > parseInt(localStorage.getItem("size"))) {
             alert("Sobre pasa los limites.Intente de nuevo")
             return
         }
         for (let index = x; index < sizeShip + x; index++) {
-            console.log(index);
             if (ContainerShips.matrixPlayer[index][y] != "a") {
                 alert("Los barcos no se pueden intersectar")
                 for (let j = x; j < index; j++) {
+                    ship.positions=[]
                     const actualDiv = document.getElementById(`p1-${j}-${y}`)
-                    actualDiv.style.backgroundColor="#e7f5ff"
+                    actualDiv.style.backgroundColor = "#e7f5ff"
                 }
                 return
             }
             const actualDiv = document.getElementById(`p1-${index}-${y}`)
             actualDiv.style.backgroundColor = color
+            ship.positions.push([index,y])
             ContainerShips.matrixPlayer[index][y] = `p1-${index}-${y}`
+            
 
 
         }
@@ -72,20 +85,19 @@ async function drawShip(div) {
             return
         }
         for (let index = y; index < sizeShip + y; index++) {
-            // console.log(ContainerShips.matrixPlayer);
 
             if (ContainerShips.matrixPlayer[x][index] != "a") {
                 alert("Los barcos no se pueden intersectar")
                 for (let j = y; j < index; j++) {
-                    
+                    ship.positions=[]
                     const actualDiv = document.getElementById(`p1-${x}-${j}`)
-
-                    actualDiv.style.backgroundColor="#e7f5ff"
+                    
+                    actualDiv.style.backgroundColor = "#e7f5ff"
 
                 }
                 return
             }
-
+            ship.positions.push([x,index])
             const actualDiv = document.getElementById(`p1-${x}-${index}`)
             actualDiv.style.backgroundColor = color
             ContainerShips.matrixPlayer[x][index] = `p1-${x}-${index}`
@@ -96,6 +108,8 @@ async function drawShip(div) {
     li.remove()
     select.remove(select.selectedIndex);
     selectColor.remove(selectColor.selectedIndex)
+    ContainerShips.ContainerUser.push(ship)
+    
     if (select.options.length == 0) {
         localStorage.setItem("map", document.getElementById("tableroConfig").innerHTML)
         document.getElementById("container").innerHTML = ""
